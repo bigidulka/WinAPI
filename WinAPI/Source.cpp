c58786f136c642a94289df61396c7059a4e0026e
Bogdan <udincevb@gmail.com>
2021-08-27 19:51:26 +0300

    Add project files.

---
+++ WinAPI/Source.cpp
@@ -0,0 +1,39 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wPrm, LPARAM lPrm);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//MessageBox(NULL, "Это мое первое окно :>", "Info - Заголовок окна", MB_ICONERROR | MB_OK | MB_HELP);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wPrm, LPARAM lPrm)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON2));
+		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+		break;
+	}
+	case WM_COMMAND:
+		switch (LOWORD(wPrm))
+		{
+		case IDOK:
+			MessageBox(NULL, "ОК", "Info", MB_ICONINFORMATION | MB_OK);
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			return TRUE;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
